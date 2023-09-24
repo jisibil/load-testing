8da9eeb6cbba33ae16cd2437c4d9bfe31bd5f333
jisibil <jessebelbelches8231@gmail.com>
2023-09-24 21:06:21 +0800

    unzip 09-24-2023 file

---
+++ 09-24-2023/resources/views/hello.blade.php
@@ -0,0 +1,49 @@
+<!-- Life is available only in the present moment. - Thich Nhat Hanh -->
+<!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f0f0f0;
+        margin: 0;
+        padding: 0;
+        }
+
+    .container {
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        border-radius: 5px;
+        text-align: center;
+        }
+
+    h1 {
+        color: #333;
+        }
+
+    p {
+        color: #666;
+        }
+
+    a {
+        color: #007bff;
+        text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Hi, I'm Jessebel.</h1>
+        <p>To go back to welcome page, just click <a href="/">HOME.</a></p>
+    </div>
+</body>
+</html>
+
---
+++ 09-24-2023/resources/views/me.blade.php
@@ -0,0 +1,47 @@
+<!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Me</title>
+    
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f0f0f0;
+        margin: 0;
+        padding: 0;
+        }
+
+    .container {
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        border-radius: 5px;
+        text-align: center;
+        }
+
+    h1 {
+        color: #333;
+        }
+
+    p {
+        color: #666;
+        }
+
+    a {
+        color: #007bff;
+        text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Hi, I'm Jessebel.</h1>
+        <p>To know about me more, just click <a href="hello">HERE.</a></p>
+    </div>
+</body>
+</html>
